ecc0ea2669aa92bc3f83e15c6d723d541ddb56a4
Колтыгин Александр Сергеевич <a.koltygin@e2e4online.ru>
2024-06-09 20:58:18 +0700

    [GOHW-2] Изменены тесты для хэндлера

--- cart/internal/app/handler/cart_handler_test.go
+++ cart/internal/app/handler/cart_handler_test.go
@@ -12,173 +12,459 @@ import (
 	productModel "route256/cart/internal/product/model"
 )
 
-func TestCartHandler(t *testing.T) {
-	ctrl := minimock.NewController(t)
-	productService := mock.NewProductServiceMock(ctrl)
-	cartService := mock.NewCartServiceMock(ctrl)
+func TestCartHandler_AddItemsToCart(t *testing.T) {
+	t.Parallel()
 
-	var (
-		err          error
-		userId       model.UserID = 1
-		cartResponse              = model.CartResponse{}
-		cartItems    []model.Item
-		cart         = &model.Cart{}
-		products     = []*productModel.Product{
-			{
-				Name:  "Item 1",
-				Price: 10,
-			},
-			{
-				Name:  "Item 2",
-				Price: 20,
-			},
-		}
-		items = []model.Item{
-			{
-				SKU:   1,
-				Name:  products[0].Name,
-				Count: 1,
-				Price: products[0].Price,
-			},
-			{
-				SKU:   2,
-				Name:  products[1].Name,
-				Count: 1,
-				Price: products[1].Price,
-			},
-			{
-				SKU:   3,
-				Count: 1,
-			},
-		}
-	)
-
-	cartHandler := NewCartHandler(cartService, productService)
-
-	cartService.GetCartByUserIDMock.Expect(userId).Return(nil, errors.New(repository.ErrCartNotFoundOrEmpty))
-
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
-	})
-	require.EqualError(t, err, repository.ErrCartNotFoundOrEmpty)
-	require.Equal(t, cartResponse, model.CartResponse{})
-
-	productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
-	cartService.AddItemsToCartMock.Expect(userId, items[0])
-
-	cart = &model.Cart{
-		Items: map[model.SkuID]*model.Item{
-			items[0].SKU: &items[0],
+	var userId model.UserID = 1
+	products := []*productModel.Product{
+		{
+			Name:  "Item 1",
+			Price: 10,
+		},
+		{
+			Name:  "Item 2",
+			Price: 20,
 		},
 	}
-	cartItems = append(cartItems, items[0])
-	cartService.GetCartByUserIDMock.Expect(userId).Return(cart, nil)
-	cartService.GetTotalPriceMock.Expect(cart).Return(uint32(10))
+	items := []model.Item{
+		{
+			SKU:   1,
+			Name:  products[0].Name,
+			Count: 1,
+			Price: products[0].Price,
+		},
+		{
+			SKU:   2,
+			Name:  products[1].Name,
+			Count: 1,
+			Price: products[1].Price,
+		},
+		{
+			SKU:   3,
+			Count: 1,
+		},
+	}
 
-	err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
-		UserID: userId,
-		SKU:    items[0].SKU,
-		Count:  items[0].Count,
+	t.Run("add new item", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
 	})
 
-	require.NoError(t, err)
+	t.Run("add existing item", func(t *testing.T) {
+		t.Parallel()
 
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
-	})
-	require.NoError(t, err)
-	require.Equal(t, cartResponse.Items, cartItems)
-	require.Equal(t, cartResponse.TotalPrice, uint32(10))
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
 
-	err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
-		UserID: userId,
-		SKU:    items[0].SKU,
-		Count:  items[0].Count,
-	})
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
 
-	require.NoError(t, err)
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
 
-	cartItems[0].Count = 2
-	cart.Items[items[0].SKU].Count = 2
-	cartService.GetCartByUserIDMock.Expect(userId).Return(cart, nil)
-	cartService.GetTotalPriceMock.Expect(cart).Return(uint32(20))
-
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
-	})
-	require.NoError(t, err)
-	require.Equal(t, cartResponse.Items, cartItems)
-	require.Equal(t, cartResponse.TotalPrice, uint32(20))
-
-	productService.GetProductMock.Expect(items[1].SKU).Return(products[1], nil)
-
-	cartItems = append(cartItems, items[1])
-	cart.Items[items[1].SKU] = &items[1]
-	cartService.GetCartByUserIDMock.Expect(userId).Return(cart, nil)
-	cartService.GetTotalPriceMock.Expect(cart).Return(uint32(40))
-
-	cartService.AddItemsToCartMock.Expect(userId, items[1])
-
-	err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
-		UserID: userId,
-		SKU:    items[1].SKU,
-		Count:  items[1].Count,
+		err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
 	})
 
-	require.NoError(t, err)
+	t.Run("add another item", func(t *testing.T) {
+		t.Parallel()
 
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		productService.GetProductMock.Expect(items[1].SKU).Return(products[1], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[1])
+
+		err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[1].SKU,
+			Count:  items[1].Count,
+		})
+		require.Nil(t, err)
 	})
-	require.NoError(t, err)
-	require.Equal(t, cartResponse.Items, cartItems)
-	require.Equal(t, cartResponse.TotalPrice, uint32(40))
 
-	productService.GetProductMock.Expect(items[2].SKU).Return(nil, errors.New("sku not found"))
+	t.Run("add item with not found sku", func(t *testing.T) {
+		t.Parallel()
 
-	err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
-		UserID: userId,
-		SKU:    items[2].SKU,
-		Count:  items[2].Count,
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[2].SKU).Return(nil, errors.New("sku not found"))
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[2].SKU,
+			Count:  items[2].Count,
+		})
+		require.EqualError(t, err, "sku not found")
 	})
-	require.EqualError(t, err, "sku not found")
+}
 
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
+func TestCartHandler_DeleteItemsFromCart(t *testing.T) {
+	t.Parallel()
+
+	var userId model.UserID = 1
+	products := []*productModel.Product{
+		{
+			Name:  "Item 1",
+			Price: 10,
+		},
+		{
+			Name:  "Item 2",
+			Price: 20,
+		},
+	}
+	items := []model.Item{
+		{
+			SKU:   1,
+			Name:  products[0].Name,
+			Count: 1,
+			Price: products[0].Price,
+		},
+	}
+
+	t.Run("delete not found item", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		cartService.DeleteItemsFromCartMock.Expect(userId, items[0].SKU)
+
+		err := cartHandler.DeleteItemsFromCart(&model.UserSKURequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+		})
+		require.Nil(t, err)
 	})
-	require.NoError(t, err)
-	require.Equal(t, cartResponse.Items, cartItems)
-	require.Equal(t, cartResponse.TotalPrice, uint32(40))
 
-	cartService.DeleteItemsFromCartMock.Expect(userId, items[0].SKU)
-	cartItems = append(cartItems[1:])
-	delete(cart.Items, items[0].SKU)
-	cartService.GetCartByUserIDMock.Expect(userId).Return(cart, nil)
-	cartService.GetTotalPriceMock.Expect(cart).Return(uint32(20))
+	t.Run("delete found item", func(t *testing.T) {
+		t.Parallel()
 
-	err = cartHandler.DeleteItemsFromCart(&model.UserSKURequest{
-		UserID: userId,
-		SKU:    items[0].SKU,
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		cartService.DeleteItemsFromCartMock.Expect(userId, items[0].SKU)
+
+		err = cartHandler.DeleteItemsFromCart(&model.UserSKURequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+		})
+		require.Nil(t, err)
 	})
-	require.NoError(t, err)
+}
 
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
+func TestCartHandler_DeleteCart(t *testing.T) {
+	t.Parallel()
+
+	var userId model.UserID = 1
+	products := []*productModel.Product{
+		{
+			Name:  "Item 1",
+			Price: 10,
+		},
+		{
+			Name:  "Item 2",
+			Price: 20,
+		},
+	}
+	items := []model.Item{
+		{
+			SKU:   1,
+			Name:  products[0].Name,
+			Count: 1,
+			Price: products[0].Price,
+		},
+	}
+
+	t.Run("delete not found cart", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		cartService.DeleteCartByUserIDMock.Expect(userId)
+
+		err := cartHandler.DeleteCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.Nil(t, err)
 	})
-	require.NoError(t, err)
-	require.Equal(t, cartResponse.Items, cartItems)
-	require.Equal(t, cartResponse.TotalPrice, uint32(20))
 
-	cartService.DeleteCartByUserIDMock.Expect(userId)
-	cartService.GetCartByUserIDMock.Expect(userId).Return(nil, errors.New(repository.ErrCartNotFoundOrEmpty))
+	t.Run("delete empty cart", func(t *testing.T) {
+		t.Parallel()
 
-	err = cartHandler.DeleteCart(&model.UserRequest{
-		UserID: userId,
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		cartService.DeleteItemsFromCartMock.Expect(userId, items[0].SKU)
+
+		err = cartHandler.DeleteItemsFromCart(&model.UserSKURequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+		})
+		require.Nil(t, err)
+
+		cartService.DeleteCartByUserIDMock.Expect(userId)
+
+		err = cartHandler.DeleteCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.Nil(t, err)
 	})
-	require.NoError(t, err)
 
-	cartResponse, err = cartHandler.GetCart(&model.UserRequest{
-		UserID: userId,
+	t.Run("delete not empty cart", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		cartService.DeleteCartByUserIDMock.Expect(userId)
+
+		err = cartHandler.DeleteCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.Nil(t, err)
 	})
-	require.EqualError(t, err, repository.ErrCartNotFoundOrEmpty)
+}
+
+func TestCartHandler_GetCart(t *testing.T) {
+	t.Parallel()
+
+	var userId model.UserID = 1
+	products := []*productModel.Product{
+		{
+			Name:  "Item 1",
+			Price: 10,
+		},
+		{
+			Name:  "Item 2",
+			Price: 20,
+		},
+	}
+	items := []model.Item{
+		{
+			SKU:   1,
+			Name:  products[0].Name,
+			Count: 1,
+			Price: products[0].Price,
+		},
+		{
+			SKU:   2,
+			Name:  products[1].Name,
+			Count: 1,
+			Price: products[1].Price,
+		},
+	}
+
+	t.Run("get not found cart", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		cartService.GetCartByUserIDMock.Expect(userId).Return(&model.Cart{}, errors.New(repository.ErrCartNotFoundOrEmpty))
+
+		cartResponse, err := cartHandler.GetCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.EqualError(t, err, repository.ErrCartNotFoundOrEmpty)
+		require.Equal(t, cartResponse, model.CartResponse{})
+	})
+
+	t.Run("get empty cart", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		cartService.DeleteItemsFromCartMock.Expect(userId, items[0].SKU)
+
+		err = cartHandler.DeleteItemsFromCart(&model.UserSKURequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+		})
+		require.Nil(t, err)
+
+		cartService.GetCartByUserIDMock.Expect(userId).Return(&model.Cart{}, errors.New(repository.ErrCartNotFoundOrEmpty))
+
+		cartResponse, err := cartHandler.GetCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.EqualError(t, err, repository.ErrCartNotFoundOrEmpty)
+		require.Equal(t, cartResponse, model.CartResponse{})
+	})
+
+	t.Run("get not empty cart", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		cartItems := make(map[model.SkuID]*model.Item)
+		cartItems[items[0].SKU] = &items[0]
+		cart := &model.Cart{
+			Items: cartItems,
+		}
+
+		cartService.GetCartByUserIDMock.Expect(userId).Return(cart, nil)
+		cartService.GetTotalPriceMock.Expect(cart).Return(10)
+
+		cartResponse, err := cartHandler.GetCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.Nil(t, err)
+		require.EqualValues(t, uint32(10), cartResponse.TotalPrice)
+		require.Len(t, cartResponse.Items, 1)
+	})
+
+	t.Run("get cart with difference items", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := minimock.NewController(t)
+		productService := mock.NewProductServiceMock(ctrl)
+		cartService := mock.NewCartServiceMock(ctrl)
+		cartHandler := NewCartHandler(cartService, productService)
+
+		productService.GetProductMock.Expect(items[0].SKU).Return(products[0], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[0])
+
+		err := cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[0].SKU,
+			Count:  items[0].Count,
+		})
+		require.Nil(t, err)
+
+		productService.GetProductMock.Expect(items[1].SKU).Return(products[1], nil)
+		cartService.AddItemsToCartMock.Expect(userId, items[1])
+
+		err = cartHandler.AddItemsToCart(&model.UserSKUCountRequest{
+			UserID: userId,
+			SKU:    items[1].SKU,
+			Count:  items[1].Count,
+		})
+		require.Nil(t, err)
+
+		cartItems := make(map[model.SkuID]*model.Item)
+		cartItems[items[0].SKU] = &items[0]
+		cartItems[items[1].SKU] = &items[1]
+		cart := &model.Cart{
+			Items: cartItems,
+		}
+
+		cartService.GetCartByUserIDMock.Expect(userId).Return(cart, nil)
+		cartService.GetTotalPriceMock.Expect(cart).Return(30)
+
+		cartResponse, err := cartHandler.GetCart(&model.UserRequest{
+			UserID: userId,
+		})
+		require.Nil(t, err)
+		require.EqualValues(t, uint32(30), cartResponse.TotalPrice)
+		require.Len(t, cartResponse.Items, 2)
+	})
 }
